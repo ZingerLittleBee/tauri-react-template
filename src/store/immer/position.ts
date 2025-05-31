d2149ee2d0bb235d09885af34ee3115c3d0861d9
ZingerLittleBee <6970999@gmail.com>
2025-05-31 15:21:13 +0800

    refactor: update position state and actions in immer store

--- src/store/immer/position.ts
+++ src/store/immer/position.ts
@@ -1,21 +1,50 @@
 import { ImmerCreator } from './immer-store'
 
-type PositionState = { position: { x: number; y: number } }
-
-type PositionActions = {
-  setPosition: ({ x, y }: { x: number; y: number }) => void
-  setPositionX: (x: number) => void
+type State = {
+  position: {
+    x: number
+    y: number
+  }
 }
 
-export type PositionSlice = PositionState & PositionActions
+type Actions = {
+  setPosition: (x: number, y: number) => void
+  setX: (x: number) => void
+  setY: (y: number) => void
+  moveBy: (deltaX: number, deltaY: number) => void
+  reset: () => void
+}
+
+export type PositionSlice = State & Actions
 
 export const createPositionSlice: ImmerCreator<PositionSlice> = (set) => ({
   position: { x: 0, y: 0 },
-  setPosition: ({ x, y }: { x: number; y: number }) =>
-    set((state) => ({ position: { ...state.position, x, y } })),
-  setPositionX: (x: number) => {
+
+  setPosition: (x: number, y: number) =>
     set((state) => {
       state.position.x = x
+      state.position.y = y
+    }),
+
+  setX: (x: number) =>
+    set((state) => {
+      state.position.x = x
+    }),
+
+  setY: (y: number) =>
+    set((state) => {
+      state.position.y = y
+    }),
+
+  moveBy: (deltaX: number, deltaY: number) =>
+    set((state) => {
+      state.position.x += deltaX
+      state.position.y += deltaY
+    }),
+
+  reset: () =>
+    set((state) => {
+      state.position.x = 0
+      state.position.y = 0
     })
-  }
 })
